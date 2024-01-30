9d71be97f036277534bee95b1e8c3f7da05be239
Mahalakshmi <mmahaece07@gmail.com>
2024-01-30 19:47:30 +0530

    FormApp created

---
+++ modals/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Model from './Model';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Model />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ modals/src/Model.js
@@ -0,0 +1,108 @@
+import React, { useState } from 'react';
+import "./Model.css"
+
+function Model()
+{
+    const[isOpen, setIsOpen] = useState(false);
+    const [username, setUsername] = useState('')
+    const [email, setEmail] = useState('');
+    const [phonenumber, setPhonenumber] = useState('');
+    const [dob, setDob] = useState('')
+
+    const openModal =() => {
+        setIsOpen(true);
+    }
+
+    const closeModal =() => {
+        setIsOpen(false);
+    }
+   
+     const handleSubmit =(e) => {
+        e.preventDefault();
+          if (e.target[2].value.length < 10) {
+            alert('Invalid phone number. Please enter a 10-digit phone number.');
+            return;
+          }
+      
+         
+      
+          if (new Date(e.target[3].value) > new Date()) {
+            alert('Invalid date of birth. Date of birth cannot be in the future.');
+            return;
+          }
+          setUsername('');
+          setEmail('');
+          setPhonenumber('');
+          setDob('');
+          setIsOpen(true);
+     }
+
+    return(
+        <div>
+          <div onClick={closeModal}></div>
+            <h1>User Details Modal</h1>
+             <button className='button' onClick={openModal}>Open Form</button>
+
+             {isOpen && (
+        <div className="modal">
+          <div className="modal-content" onClick={(e) => e.stopPropagation()} >
+            <h2>Fill Details</h2>
+            <form onSubmit={handleSubmit} >
+                <div className='form-group'>
+              <label htmlFor="username">Username:</label>
+              <input
+                type="text"
+                id="username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                required
+              />
+              </div>
+
+
+              <div className='form-group'>
+              <label htmlFor="email">Email Address:</label>
+              <input
+                type="email"
+                id="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                required
+              />
+              </div>
+              
+
+              <div className='form-group'>
+              <label htmlFor="phonenumber">Phone Number:</label>
+              <input
+                type="text"
+                id="phonenumber"
+                value={phonenumber}
+                onChange={(e) => setPhonenumber(e.target.value)}
+                required
+              />
+              </div>
+
+
+              <div className='form-group'>
+              <label htmlFor="dob">Date of Birth:</label>
+              <input
+                type="date"
+                id="dob"
+                value={dob}
+                onChange={(e) => setDob(e.target.value)}
+                required
+              />
+               </div>
+
+              <button type="submit" className="submit-button">
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      )}
+        </div>
+    )
+}
+export default Model;
